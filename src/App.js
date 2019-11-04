d204b481889465c317595e7657f7b53689381cf3
Joel Biffin <joel.biffin@onthebeach.co.uk>
2019-11-04 12:11:48 +0000

    intitial basic setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>On the Beach Grad App</h1>
+      </header>
+      <nav>
+        <a href="#">Past</a>
+        <a href="#">Present</a>
+        <a href="#">Future</a>
+        <a href="#">Induction</a>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
